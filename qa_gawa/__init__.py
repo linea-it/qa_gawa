92d0ea329c98e0531ee33118dd1a0d701ddcc509
adrianopieres <adriano.pieres@gmail.com>
2022-08-08 14:33:29 -0300

    Init repo profile (#1)
    * First commit with code and data.
    
    * Initial repository to the code that fits the profile.
    
    * First creation of jn to run the code.
    
    * Creating a jn to fit the clusters.
    
    * Inserting information about how to run the code in Lineas env.
    
    * Adding instructions to create env on Linea.
    
    * Adding code to plot profiles and a few changes on how the stars are being accounted.
    
    * Correcting place of folder to create env.
    
    * Inserting a few changes in the code and optimized pars for MCMC.
    
    * Updating file writing results to file.
    
    * Creating a cutout to evaluate likelihood in order to speed up computational run.
    
    * Adding some documentation format.
    
    * Adding some documentation format.
    
    * Testing and complementing documentation.
    
    * Testing and fixing details in code in order to run on Lineas jn.
    
    * Adding plots to compare truth table to the fits from emcee.
    
    * Fixing code regarding radial units.
    
    * Inserting more comments and fine tunning program to fit simulated clusters.
    
    * Inserting Nstars and plots. Also adding a new file with simulated clusters, correcting the exponential profile.
    
    * Adding correct data files and upgrading the plots.
    
    * Initial commit of King profile fit.
    
    * Initial commit of King profile fit.
    
    * Updating data files with another simulation.
    
    * Tradução dos graficos de pureza e adição de comentarios
    
    * Atualização da pureza com a tradução e comentarios
    
    * Changes in markdowns and comments.
    
    * Adding a JSON file as a configuration file and a few settings to run fittings globally.
    
    * Implementing comparison with N stars filtered and a few fixes.
    
    * Updating code to run and generate an html page as report.
    
    * Updating code to run on simulations stored on processes folder.
    
    * Adding a few files to create a library to qa_gawa.
    
    * Updating code to match detections and simulations.
    
    * Joining match, completeness and purity in a single jn.
    
    * Small changes in plots and setting max to completeness == 1..
    
    * Updating plots with many functions.
    
    * Updating plots.
    
    * Updating and improving plots.
    
    * Updating plots
    
    * Updating code to plot profile of simulated clusters.
    
    * Updating plots.
    
    * Updating jn with more plots to show undetected clusters.
    
    * Adding more plots to the comparison of undetected clusters. Adding real objects to the completeness 2D plot and a few fixes.
    
    * Updating plots creating functions in library
    
    * Converting many jn code as functions on library.
    
    * Inserting documentation to the functions.
    
    * Updating jn afeter a review. A few fixes regarding the arguments of functions.
    
    * Improvements in plots and inserting an initial documentation of the code.
    
    Co-authored-by: Apleiade <moreira1@usp.br>

---
+++ qa_gawa/__init__.py
@@ -0,0 +1,23 @@
+from qa_gawa.qa_gawa import (
+    plot_pure,
+    plot_comp,
+    full_completeness_distances,
+    plot_clus_position,
+    plots_ang_size,
+    general_plots,
+    recursive_print_dict,
+    plot_masks,
+    matching_sim_det,
+    write_det_numbers,
+    SNR_hist,
+    dist_hist,
+    det_sky,
+    dist_dist,
+    SNR_SNR,
+    puri_comp,
+    plot_pure_mM,
+    plot_pure_SNR,
+    plot_comp_all,
+    print_undet_table,
+    undet_cmds
+)
